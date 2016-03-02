f26db797286c7383dc6db697399d4d8dc34e20a0
w0ng3r <w0ng3r@gmail.com>
2016-03-02 15:08:39 -0800

    head footer

---
+++ footer_inc.php
@@ -0,0 +1,35 @@
+      <footer>
+        <div class="row">
+          <div class="col-lg-12">
+            
+            <ul class="list-unstyled">
+              <li class="pull-right"><a href="#top">Back to top</a></li>
+            </ul>
+              
+              
+			<p><?=$config->copyright;?>. Theme by <a href="http://bootswatch.com/">Bootswatch</a>, based on <a href="http://getbootstrap.com/css/">Bootstrap</a>.
+            </p>
+            
+            <div>
+                <h2>Contact G &amp; G's RSS News Feeds</h2>
+				<h3>Call 0-123-456-789</h3>
+                <a href="w0ng3r.dlinkddns.com/app/RSSViewer.php">G &amp;G</a>
+            <a href="rss.com" target="_blank">RSS</a>
+<span>&copy; 2016 rssviewer.com. All Rights Reserved - Authored by George and Grace</span>
+            <a href="http://phpcodechecker.com" target="_blank">Valid PHP</a> ~ 
+            </div>
+
+          </div>
+        </div>
+        
+      </footer>
+    
+
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="<?=THEME_PATH;?>js/bootstrap.min.js"></script>
+    <script src="<?=THEME_PATH;?>js/bootswatch.js"></script>
+  </body>
+</html>
