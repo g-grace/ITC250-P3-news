1a8eb9affdeac8c7e3b9ab4b235e642b6ca93a13
g-grace <g-grace@users.noreply.github.com>
2016-03-07 17:02:45 -0800

    Update NewsItem.php

--- NewsItem.php
+++ NewsItem.php
@@ -1,3 +1,34 @@
+/**
+ * NewsItem.php is the Class using to create objects for the RSS-Feeds App as news' items with Title, Description,  URL, date, image fields
+ * 
+ * In addition it shows images from the feeds
+ *
+ * @package RSS-FEEDS
+ * @authorA: George Wong: <w0ng3r@gmail.com>
+ * @authorB: Grace Sangonpiyaphan: <ggrace5599@gmail.com>
+ * @version 1.0 2015/04/30 
+ * @link http://w0ng3r.dlinkddns.com/app/RSSViewer.php
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @see RSSViewer.php
+ * @see admin_add.php
+ * @see admin_dashboard.php
+ * @see admin_edit.php
+ * @see admin_info.php
+ * @see admin_log_list.php
+ * @see admin_log_view.php
+ * @see admin_login.php
+ * @see admin_logout.php
+ * @see admin_reset.php
+ * @see admin_validate.php
+ * @see adminer.php
+ * @see header_inc.php
+ * @see footer_inc.php
+ * @see index.php
+ * @see loadRSS.php
+ * @see simple.php
+ * @todo none
+ */
+
 <?php 
 Class NewsItem
 {
