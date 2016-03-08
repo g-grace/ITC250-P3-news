bc11f59b7115dbd55443bf3ee74c822d42bf0032
g-grace <g-grace@users.noreply.github.com>
2016-03-07 17:39:24 -0800

    Update admin_dashboard.php
    updated on PHPDocumentation

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -5,10 +5,11 @@
  * Use this file as a landing page after successfully logging in as an administrator.  
  * Be sure this page is not publicly accessible by referencing admin_only_inc.php
  * 
- * @package nmAdmin
- * @author Bill Newman <williamnewman@gmail.com>
- * @version 2.14 2012/10/01
- * @link http://www.newmanix.com/
+ * @package RSS-FEEDS
+ * @authorA: George Wong: <w0ng3r@gmail.com>
+ * @authorB: Grace Sangonpiyaphan: <ggrace5599@gmail.com>
+ * @version 1.0 2015/04/30 
+ * @link http://w0ng3r.dlinkddns.com/app/RSSViewer.php
  * @license http://opensource.org/licenses/osl-3.0.php Open Software License ("OSL") v. 3.0
  * @see $config->adminLogin.php
  * @see $config->adminValidate.php
