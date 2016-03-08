0dedd2445a5837d5b608a1ff9ff20991a1c4e366
g-grace <g-grace@users.noreply.github.com>
2016-03-07 17:58:00 -0800

    Update footer_inc.php
    updated PHPDocu

--- footer_inc.php
+++ footer_inc.php
@@ -1,3 +1,36 @@
+/**
+* footer_inc.php 
+* Establish footer where navigate to filter on Source and Category of the RSS-FEEDS, website, contact, code checker
+* Copyright of the theme
+*
+* @package RSS-FEEDS
+* @authorA: George Wong: <w0ng3r@gmail.com>
+* @authorB: Grace Sangonpiyaphan: <ggrace5599@gmail.com>
+* @version 1.0 2015/04/30 
+* @link http://w0ng3r.dlinkddns.com/app/RSSViewer.php
+* @license http://www.apache.org/licenses/LICENSE-2.0
+* @see RSSViewer.php
+* @see SQL.sql
+* @see inc_0700/config_inc.php
+* @see admin_add.php
+* @see admin_dashboard.php
+* @see admin_edit.php
+* @see admin_info.php
+* @see admin_log_list.php
+* @see admin_log_view.php
+* @see admin_login.php
+* @see admin_logout.php
+* @see admin_reset.php
+* @see admin_validate.php
+* @see adminer.php
+* @see header_inc.php
+* @see footer_inc.php
+* @see index.php
+* @see loadRSS.php
+* @see simple.php
+* @todo none
+*/
+      
       <footer>
         <div class="row">
           <div class="col-lg-12">
