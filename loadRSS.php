ad6cb12f52ef50f0a27aea45ad1bb6a66aff45e5
g-grace <g-grace@users.noreply.github.com>
2016-03-07 18:03:27 -0800

    Update loadRSS.php
    updated PHPDocu

--- loadRSS.php
+++ loadRSS.php
@@ -4,7 +4,32 @@
 **
 **This file will load all the latest stories from the list of rss feeeds (listed below), 
 **
-**
+* @package RSS-FEEDS
+* @authorA: George Wong: <w0ng3r@gmail.com>
+* @authorB: Grace Sangonpiyaphan: <ggrace5599@gmail.com>
+* @version 1.0 2015/04/30 
+* @link http://w0ng3r.dlinkddns.com/app/RSSViewer.php
+* @license http://www.apache.org/licenses/LICENSE-2.0
+* @see RSSViewer.php
+* @see SQL.sql
+* @see inc_0700/config_inc.php
+* @see admin_add.php
+* @see admin_dashboard.php
+* @see admin_edit.php
+* @see admin_info.php
+* @see admin_log_list.php
+* @see admin_log_view.php
+* @see admin_login.php
+* @see admin_logout.php
+* @see admin_reset.php
+* @see admin_validate.php
+* @see adminer.php
+* @see header_inc.php
+* @see footer_inc.php
+* @see index.php
+* @see loadRSS.php
+* @see simple.php
+* @todo none
 */
 //simple.php is the all-in-one php file for simplepie, a tool that parses rss feeds
     require 'include\simple.php';
