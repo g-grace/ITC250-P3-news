b3834f81e29b45234ecdbd1edf5f90f4bba60859
g-grace <g-grace@users.noreply.github.com>
2016-03-07 17:46:28 -0800

    Update admin_login.php
    updated PHPDocu

--- admin_login.php
+++ admin_login.php
@@ -8,10 +8,11 @@
  * 6/4/12 - added session destroy after being passed from logout due to session var 
  * needed for feedback() 
  *
- * @package nmAdmin
- * @author Bill Newman <williamnewman@gmail.com>
- * @version 2.014 2012/06/09
- * @link http://www.newmanix.com/ 
+ * @package RSS-FEEDS
+ * @authorA: George Wong: <w0ng3r@gmail.com>
+ * @authorB: Grace Sangonpiyaphan: <ggrace5599@gmail.com>
+ * @version 1.0 2015/04/30 
+ * @link http://w0ng3r.dlinkddns.com/app/RSSViewer.php
  * @license http://opensource.org/licenses/osl-3.0.php Open Software License ("OSL") v. 3.0
  * @see admin_validate.php
  * @see admin_dashboard.php
